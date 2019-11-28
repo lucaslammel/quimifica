9f525a6d8b0189d61a504c5d5936b2a974611410
Felipe Souza <felipessouza0703@gmail.com>
2019-11-27 22:54:31 -0300

    nomes dos conteudos

--- database/seeds/conteudoTableSeeder.php
+++ database/seeds/conteudoTableSeeder.php
@@ -18,4 +18,16 @@ class conteudoTableSeeder extends Seeder
         'descricao' => 'A Química Orgânica estuda a composição e as propriedades dos compostos que apresentam o carbono como principal elemento químico.',
         'user_id' => '1']);
     }
+    {
+        conteudo::create([
+        'titulo' => 'Química Geral',
+        'descricao' => 'Química é a ciência que estuda a estrutura das substâncias, a composição e as propriedades das diferentes matérias, suas transformações e variações de energia.',
+        'user_id' => '1']);
+    }
+    {    
+        conteudo::create([
+        'titulo' => 'Físico-química',
+        'descricao' => 'A físico-química é a disciplina que estuda as propriedades físicas e químicas da matéria, através da combinação de duas ciências: a física (onde se destacam áreas como a termodinâmica e a mecânica quântica) e a química.',
+        'user_id' => '1']);
+    }
 }
